f445313f4399b04b2fd54f002e7575e4782bb6b6
Piyush Gwayamaru <shresthpiyusha@gmail.com>
2025-03-21 13:17:45 +0545

    folder updated

---
+++ newtest/testing.py
@@ -0,0 +1,10 @@
+def multiply_numbers(a, b):
+    return a * b
+
+# Taking input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calling the function and displaying the result
+result = multiply_numbers(num1, num2)
+print("Product:", result)
